02ca1da0b66758b345340a915f1152293fe6261d
mpuss37 <herdiseptanto2017@gmail.com>
2024-08-23 10:29:14 +0700

    target : finish easy options for read or write issue : i just confuse for implement logic easy to read update : finish custom arguments on class Main.java

--- src/main/java/org/master/Main.java
+++ src/main/java/org/master/Main.java
@@ -1,79 +1,10 @@
 package org.master;
 
 public class Main {
-    static String number;
-    static String adjustCase;
-    static String symbol;
-    static boolean choice;
-    static String args0;
-    static PassGen passGen = new PassGen();
+    static PassOptions passOptions = new PassOptions();
 
     public static void main(String[] args) {
-        try {
-            args0 = args[0];
-            if (args0.startsWith("-")) {
-                if (args0.contains("l") || args0.contains("length")) {
-                    if (args.length == 2 || args.length == 3) {
-                        try {
-                            passGen.lengthPass = Integer.parseInt((args[1]));
-                            choice = true;
-                        } catch (NumberFormatException e) {
-                        }
-                    } else {
-                        passGen.lengthPass = 8;
-                    }
-                }
-                if (args0.contains("s") || args0.contains("symbols")) {
-                    symbol = passGen.setRandomText("", "symbols", passGen.lengthPass);
-                    choice = true;
-                    if (symbol.equals(null)) {
-                        symbol = "";
-                    }
-                }
-                if (args0.contains("n") || args0.contains("number")) {
-                    number = passGen.setRandomText("", "number", passGen.lengthPass);
-                    if (number.equals(null)) {
-                        number = "";
-                    }
-                }
-                if (args0.contains("u")) {
-                    if (args.length == 3) {
-                        adjustCase = String.valueOf(((args[2])));
-                        choice = true;
-                    } else if (args.length == 2) {
-                    } else {
-                        adjustCase = "";
-                    }
-                }
-                if (args0.contains("h") || args0.contains("help")) {
-                    optionsMenu();
-                    choice = false;
-                }
-                if (choice) {
-                    passGen.randomText += passGen.ALPHABET += symbol += number;
-                    passGen.finalText = passGen.setRandomText(passGen.randomText, "", passGen.lengthPass);
-                    passGen.choiceCase = passGen.setAdjustCase(adjustCase, passGen.finalText);
-                    System.out.println(passGen.choiceCase);
-                }
-            } else {
-                System.out.println("kuncen-gen: missing operand\n" + "Try 'kuncen-gen -h or --help' for more information.");
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-        }
-    }
-
-    static void optionsMenu() {
-        System.out.println("kuncen-gen (version 1.0, revision 1)");
-        System.out.println("""
-                Usage:
-                 kuncen-gen [CUSTOM_OPTIONS]...[VALUES]\t
-                 Example: kuncen-gen -lcns '10' upper
-                  -l, --length [values]    specified length default 8.
-                  -c, --case [upper/lower]     adjust case upper or lower.
-                  -n, --numeric     with numeric digits.
-                  -s, --symbols [custom_char]     with symbol can add custom char.
-                  -e, --easy [read/write]     easy to read or write pass.
-                  -h, --help     for check options and information.
-                """);
+        //calling method process args for can get indexing args
+        passOptions.processArgs(args);
     }
 }
--- src/main/java/org/master/PassOptions.java
+++ src/main/java/org/master/PassOptions.java
@@ -1,4 +1,110 @@
 package org.master;
 
 public class PassOptions {
+    PassGen passGen = new PassGen();
+    String number;
+    String symbol;
+    String adjustCase;
+    boolean choice;
+
+    /*for can indexing args with start char '-' and if false can print error msg
+    bassed on indexing args if lower on zero / 0 will print error msg*/
+    public void processArgs(String[] args) {
+        try {
+            /*convert array args to string*/
+            String args0 = args[0];
+            /* args zero / 0 start with char '-' */
+            if (args0.startsWith("-")) {
+                handleOptions(args, args0);
+            } else {
+                System.out.println("kuncen-gen: missing operand\n" + "Try 'kuncen-gen -h or --help' for more information.");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Invalid arguments. Try 'kuncen-gen -h or --help' for more information.");
+        }
+    }
+
+    private void handleOptions(String[] args, String args0) {
+        if (args0.contains("l") || args0.contains("length")) {
+            handleLengthOption(args);
+        }
+        if (args0.contains("s") || args0.contains("symbols")) {
+            handleSymbolsOption();
+        }
+        if (args0.contains("n") || args0.contains("number")) {
+            handleNumberOption();
+        }
+        if (args0.contains("u")) {
+            handleAdjustCase(args);
+        }
+        if (args0.contains("h") || args0.contains("help")) {
+            menu();
+            choice = false;
+        }
+        if (choice) {
+            generatePassword();
+        }
+    }
+
+    private void generatePassword() {
+        passGen.randomText += passGen.ALPHABET + symbol + number;
+        passGen.finalText = passGen.setRandomText(passGen.randomText, "", passGen.lengthPass);
+        passGen.choiceCase = passGen.setAdjustCase(adjustCase, passGen.finalText);
+        System.out.println(passGen.choiceCase);
+    }
+
+    private void handleLengthOption(String[] args) {
+        if (args.length == 2 || args.length == 3) {
+            try {
+                passGen.lengthPass = Integer.parseInt(args[1]);
+                choice = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid length provided, using default length of 8.");
+                choice = true;
+                passGen.lengthPass = 8;
+            }
+        } else {
+            choice = true;
+            passGen.lengthPass = 8;
+        }
+    }
+
+    private void handleSymbolsOption() {
+        symbol = passGen.setRandomText("", "symbols", passGen.lengthPass);
+        choice = true;
+        if (symbol == null) {
+            symbol = "";
+        }
+    }
+
+    private void handleNumberOption() {
+        number = passGen.setRandomText("", "number", passGen.lengthPass);
+        if (number == null) {
+            number = "";
+        }
+    }
+
+    private void handleAdjustCase(String[] args) {
+        if (args.length == 3) {
+            adjustCase = args[2];
+            choice = true;
+        } else if (args.length == 2) {
+            // If there's no additional argument after -u
+            adjustCase = "";
+        }
+    }
+
+    public void menu() {
+        System.out.println("kuncen-gen (version 1.0, revision 1)");
+        System.out.println("""
+                Usage:
+                 kuncen-gen [CUSTOM_OPTIONS]...[VALUES]\t
+                 Example: kuncen-gen -lcns '10' upper
+                  -l, --length [values]    specified length default 8.
+                  -c, --case [upper/lower]     adjust case upper or lower.
+                  -n, --numeric     with numeric digits.
+                  -s, --symbols [custom_char]     with symbol can add custom char.
+                  -e, --easy [read/write]     easy to read or write pass (soon).
+                """);
+    }
 }
